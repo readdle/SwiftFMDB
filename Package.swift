5dfb8790013cb4b6eebc83eb4b6f5fae68d80383
Andrew Druk <adruk@readdle.com>
2023-12-04 15:21:02 +0200

    System: update sqlite-ee

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.4")),
-        .package(name: "SQLiteEE", url: "git@github.com:readdle/swift-sqlite-ee.git", .branch("feature/android-swift-5.7"))
+        .package(name: "SQLiteEE", url: "git@github.com:readdle/swift-sqlite-ee.git", .branch("3.39.4-readdle.1"))
     ],
     targets: [
         .target(name: "SwiftFMDB",
