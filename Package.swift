a61a4586d65f0ce2ce663c8664577b2738db09a9
Andrew Druk <adruk@readdle.com>
2024-04-03 22:51:34 +0300

    System: update sqlite with unicode fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMinor(from: "1.4.4")),
-        .package(name: "SQLiteEE", url: "git@github.com:readdle/swift-sqlite-ee.git", .branch("3.39.4-readdle.1"))
+        .package(name: "SQLiteEE", url: "git@github.com:readdle/swift-sqlite-ee.git", .branch("3.39.4-readdle.2"))
     ],
     targets: [
         .target(name: "SwiftFMDB",
